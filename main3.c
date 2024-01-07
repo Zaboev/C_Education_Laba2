cf7ee6a1b5cc1f57631cf19b4886f007848846f2
Zaboev <148198802+Zaboev@users.noreply.github.com>
2024-01-07 12:33:24 +0300

    Add files via upload

--- main3.c
+++ main3.c
@@ -1,75 +1,92 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
+#include <stdlib.h>
 
-void search_string(char* search, int files, ...)
+int* Search_Line(char*, int, ...);
+
+int main(int argc, const char* argv[])
 {
-    va_list remnant;
-    char* file_name;
-    int i, j;
-    char a;
-    int line = 1;
-    int word_num = 1;
-    int word_result, helpy;
-    int search_len = strlen(search);
-    va_start(remnant, files);
-    for (i = 0; i < files; i++)
-    {
-        FILE* file;
-        file_name = va_arg(remnant, char*);
-        if ((file = fopen(file_name, "r")) != NULL)
-        {
-            word_num = 1;
-            line = 1;
-            helpy = 0;
-            while ((a = fgetc(file)) != EOF)
-            {
-                if (a == '\n')
-                {
-                    line++;
-                    word_num = 1;
-                }
-                else if (a == *search)
-                {
-                    word_result = word_num + 1;
-                    j = 0;
-                    while (helpy < search_len && (a == *(search + j)))
-                    {
-                        a = fgetc(file);
-                        j++;
-                        helpy++;
-                        word_num++;
-                    }
-                    if (helpy == search_len)
-                    {
-                        printf("From file %s we found in %d line by %d symbol\n", file_name, line, word_result - search_len + 1);
-                        helpy = 0;
-                    }
-                    else
-                    {
-                        word_num -= helpy;
-                        helpy = 0;
-                    }
-                }
-                else
-                {
-                    word_num++;
-                }
-            }
-            fclose(file);
-        }
-        else
-        {
-            printf("Hi\n");
-            perror("E");
-        }
+    int i, number_row, number_symbol, number_file, * values, * valuesPointer;
+    values = Search_Line("star", 1, "k1ng.txt");
+    valuesPointer = values;
+    if (values == NULL)
+    { 
+        printf("Arguments invalid or file can not be open");
+        return 1;
     }
-    va_end(remnant);
+    for (i = 1; *valuesPointer != 0; i++)
+    {
+        number_file = *valuesPointer++;
+        number_row = *valuesPointer++;
+        number_symbol = *valuesPointer++;
+        printf("Count number %d in file %d: row %d, symbol %d\n", i, number_file, number_row, number_symbol);
+    }
+    if (values != NULL)
+    {
+        free(values);
+    }
+    return 0;
 }
 
-int main()
+int* Search_Line(char* const str, int count, ...)
 {
-    search_string("star", 1, "k1ng.txt");
-    return 0;
+    int* values, *valuesPointer;
+    va_list ap;
+    FILE* file;
+    char ch, * strPointer = str;
+    int i, findedIndex = -1, number_row = 1, number_symbolInRow = 0;
+    if (count < 1) 
+    {
+        return NULL;
+    }
+    values = calloc(BUFSIZ, sizeof(char));
+    if (values == NULL) 
+    {
+        return NULL;
+    }
+    valuesPointer = values;
+    va_start(ap, count);
+    for (i = 0; i < count; i++)
+    {
+        if (!(file = fopen(va_arg(ap, char*), "r")))
+        {
+            continue;
+        }
+        strPointer = str;
+        findedIndex = -1;
+        number_row = 1;
+        number_symbolInRow = 0;
+        while ((ch = fgetc(file)) != EOF)
+        {
+            if (ch == '\n')
+            {
+                number_symbolInRow = 0;
+                number_row++;
+                findedIndex = -1;
+                strPointer = str;
+                continue;
+            }
+            number_symbolInRow++;
+            if (ch == *strPointer++)
+            {
+                if (findedIndex == -1) findedIndex = number_symbolInRow;
+            }
+            else
+            {
+                strPointer = str;
+                findedIndex = -1;
+            }
+            if (*strPointer == '\0')
+            {
+                *valuesPointer++ = (i + 1);
+                *valuesPointer++ = number_row;
+                *valuesPointer++ = findedIndex;
+                strPointer = str;
+                findedIndex = -1;
+            }
+        }
+        fclose(file);
+    }
+    va_end(ap);
+    return values;
 }
